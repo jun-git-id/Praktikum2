e653b046e7ea77eb656bb231a83b434ca53a3212
Fahmi <junanda06@gmail.com>
2020-11-07 20:11:13 +0700

    Perubahan lagi

--- application/views/v_homepage.php
+++ application/views/v_homepage.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pemograman Web 2</title>
+    <title>Praktikum 2</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/animate.css">
 </head>
